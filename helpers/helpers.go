60eb4da4a6174c0c1ad49691056c41eb56d7ddc4
Tim Scheuermann <tim.scheuermann@msasafety.com>
2016-02-17 11:29:05 +0100

    Fixed linter issues in helper.go

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,32 +1,5 @@
-// helper.go
-// Helper routines for tinkerforge
+// Package helpers has helper routines for tinkerforge
 // Author: Tim Scheuermann (https://github.com/noxer)
-//
-// License:
-// Copyright (c) 2014, Tim Scheuermann
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice,
-// this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-// this list of conditions and the following disclaimer in the documentation
-// and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
 package helpers
 
 import (
@@ -102,7 +75,7 @@ var (
 	}
 )
 
-// Provides identity information for a bricklet
+// BrickletIdentity holds identity information of a bricklet
 type BrickletIdentity struct {
 	UID              string
 	ConnectedUID     string
@@ -112,6 +85,7 @@ type BrickletIdentity struct {
 	DeviceIdentifier uint16
 }
 
+// GetIdentity queries idenitity information from a bricklet
 func GetIdentity(t tinkerforge.Tinkerforge, uid uint32) (*BrickletIdentity, error) {
 
 	p, err := tinkerforge.NewPacket(uid, 255, true)
@@ -138,6 +112,7 @@ func GetIdentity(t tinkerforge.Tinkerforge, uid uint32) (*BrickletIdentity, erro
 	return i, nil
 }
 
+// DeviceName translates the device ID into a human readable name
 func (i *BrickletIdentity) DeviceName() string {
 	return DeviceIdentifiers[i.DeviceIdentifier]
 }
